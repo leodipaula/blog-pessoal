97a0e85ee76b7c5d8110322022515ad8d8b3f0e2
leodipaula <leofernandes9@gnail.com>
2025-04-23 22:24:19 -0300

    feat: Spring Security e JWT

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -37,4 +37,8 @@ public class Postagem {
     @ManyToOne
     @JsonIgnoreProperties("postagem")
     private Tema tema;
+
+    @ManyToOne
+    @JsonIgnoreProperties("postagem")
+    private Usuario usuario;
 }
